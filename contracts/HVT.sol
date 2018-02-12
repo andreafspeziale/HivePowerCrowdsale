07cebd5b45cf16fca8fc45b00edf7e25c0050087
Davide Strepparava <davide.strepparava@supsi.ch>
2018-02-12 14:49:24 +0100

    Intergrated last Openzeppelin version for SafeMath and Ownable

--- contracts/HVT.sol
+++ contracts/HVT.sol
@@ -1,7 +1,7 @@
 pragma solidity 0.4.18;
 
-import './SafeMath.sol';
-import './Ownable.sol';
+import 'zeppelin-solidity/contracts/math/SafeMath.sol';
+import 'zeppelin-solidity/contracts/ownership/Ownable.sol';
 import './MintableInterface.sol';
 import './StandardToken.sol';
 
