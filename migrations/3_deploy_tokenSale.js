964883ed59853ec8fd8657e24331807fa5dcc741
aspeziale <andreaf.speziale@gmail.com>
2018-05-23 12:37:11 +0200

    Replicate development migration phase for the Eidoo network using the correct KYC signers

--- migrations/3_deploy_tokenSale.js
+++ migrations/3_deploy_tokenSale.js
@@ -33,6 +33,33 @@ module.exports = function(deployer, network, accounts) {
 
     //kyc signers
     var kycSigners = [accounts[2], accounts[3]];
+  } else if (network == 'eidoo') {
+    // Wallet
+    var wallet = accounts[1];
+
+    var initialDelay = web3.eth.getBlock(web3.eth.blockNumber).timestamp + (3600 * 1);
+
+    var startTime = initialDelay; // ICO starting 1 hour after the initial deployment
+    var endTime = startTime + (3600 * 2); // ICO end
+
+    var etherPriceUSD = 500;
+    var rate1 = parseInt((etherPriceUSD / 0.25) * 1.3);
+    var rate2 = parseInt((etherPriceUSD / 0.25) * 1.1);
+    var rate3 = parseInt((etherPriceUSD / 0.25) * 1.0);
+
+    var cap1 = 10 * 1e6 * 1e18;
+    var cap2 = 25 * 1e6 * 1e18;
+    var cap3 = 50 * 1e6 * 1e18;
+
+    var foundersTokens = 10 * 1e6 * 1e18;
+    var stepLockedToken = 86400 * 30 * 6;
+    var additionalTokens = 40 * 1e6 * 1e18;
+    var goal = parseInt(1000000 / etherPriceUSD * 1e18);
+
+    var overshoot = web3.toWei(5, 'ether');
+
+    //kyc signers
+    var kycSigners = [stagingKYCSigners[0], stagingKYCSigners[1]];
   } else if (network == 'ropsten') {
     // Wallet
     var wallet = accounts[1];
